feba3c8a0273022a5f11dac71a489dece908ae2d
Nika Zhenya <nika.zhenya@gmail.com>
2016-12-14 08:28:35 -0600

    Adición de estilos y de una opción

--- EPUB/Scripts-fijo/zoom.js
+++ EPUB/Scripts-fijo/zoom.js
@@ -5,6 +5,9 @@ var zoom = {
     //  Indica si restaurar el zoom por defecto
     restaurar: false,
 
+    //  Indica si también se hay zoom en versiones de escritorio
+    computadora: false,
+
     //  Para añadir animacion
     animacion: {
         "mostrar": true,
@@ -115,16 +118,38 @@ var zoom = {
             zoomMenor = document.createElement("p"),
             zoomMayor = document.createElement("p"),
             separador = document.createElement("p"),
-            margen = 3;  // Ojo: hay dispositivos que los lados se utilizan para cambiar de página, disminuir este espacio puede ocasionar conflictos
+            margen = 3, // Ojo: hay dispositivos que los lados se utilizan para cambiar de página, disminuir este espacio puede ocasionar conflictos
+            m = movil();
+
+        //  De: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser#11381730
+        function movil () {
+            var check = false;
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            return check;
+        };
+
+        //  Aborta la creación si no es un dispositivo móvil y no se especificó que fuera en cualquier dispositivo
+        if (!m && !zoom.computadora)
+            return;
 
         function estilo (dom) {
             dom.style.fontFamily = "Tahoma, Geneva, sans-serif";
             dom.style.display = "inline";
             dom.style.cursor = "pointer";
+            dom.style.color = "gray";
             dom.style.webkitUserSelect = "none";
             dom.style.MozUserSelect = "none";
             dom.style.msUserSelect = "none";
             dom.style.userSelect = "none";
+
+            //  Para evitar que la hoja de estilo modifique el aspecto
+            dom.style.margin = "0";
+            dom.style.padding = "0";
+            dom.style.border = "0";
+            dom.style.lineHeight = "1.25";
+            dom.style.fontSize = "1em";
+            dom.style.verticalAlign = "baseline";
+            dom.style.textAlign = "justify";
         }
 
         //  Oculta el body porque la animación es para mostrarlo
@@ -155,14 +180,14 @@ var zoom = {
         document.body.appendChild(fuente);
 
         //  Estilos
-        document.body.style.overflow = "hidden";
+        estilo(zoomMenor);
+        estilo(zoomMayor);
+        estilo(separador);
 
-        contenedor.style.overflow = "auto";
-        contenedor.style.width = "100%";
-        contenedor.style.height = "calc(100vh - " +          // Altura = alto de la ventana
-            fuente.offsetHeight + "px - " +                                         // - alto de los botones
-            parseFloat(window.getComputedStyle(document.body)["margin-bottom"]) +   // - margen inferior del body
-            "px)";
+        zoomMayor.style.fontSize = "1.5em";
+
+        separador.style.fontSize = "1.5em";
+        separador.style.margin = "auto .25em";
 
         fuente.style.position = "fixed";
         fuente.style.top = margen + "em";
@@ -171,27 +196,28 @@ var zoom = {
         fuente.style.textAlign = "right";
         fuente.style.padding = ".5em";
         fuente.style.borderRadius = ".5em";
-        fuente.style.color = "gray";
         fuente.style.backgroundColor = "white";
 
-        separador.style.fontSize = "1.5em";
-        separador.style.margin = "auto .25em";
+        contenedor.style.overflow = "auto";
+        contenedor.style.width = "100%";
+        contenedor.style.height = "calc(100vh - " +                                 // Altura = alto de la ventana
+            fuente.getBoundingClientRect().bottom + "px - " +                       // - posición y inferior de los botones
+            parseFloat(window.getComputedStyle(document.body)["margin-bottom"]) +   // - margen inferior del body
+            "px)";
+        contenedor.style.marginTop = fuente.getBoundingClientRect().bottom + "px";  // Margen superior = posición y inferior de los botones
 
-        zoomMayor.style.fontSize = "1.5em";
-
-        estilo(zoomMenor);
-        estilo(zoomMayor);
-        estilo(separador);
+        document.body.style.overflow = "hidden";
 
         /*  La estructura generada es (ignorando los estilos directos):
             <div id="contenedor">
-                ... [Contenido del libro]
+                … [Contenido del libro]
             </div>
             <div id="fuente">
                 <p id="zoom-menor">A</p>
                 <p id="separador">|</p>
                 <p id="zoom-mayor">A</p>
             </div>
+            <style>…</style>
         */
 
         //  Cuando se presionen los botones se ejecutará la función «zoom»
@@ -211,6 +237,7 @@ var zoom = {
 
 //  Todo empezará hasta que se cargue el DOM
 window.onload = function () {
+    // zoom.computadora = true;    // Descomentar si también se quiere aplicar a computadoras
     // zoom.animacion.mostrar = false;    // Descomentar si no se desea animación
     // zoom.restaurar = true;  // Descomentar si siempre se desea empezar con el zoom por defecto
     zoom.crear();
