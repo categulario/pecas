91b4d26134a5c459af711f6c60caaf933483ddfc
NikaZhenya <nika-zhenya@archlinux.localdomain>
2017-02-08 22:04:04 -0600

    Adición de cabeza para archivo HTML

--- otros/secundarios/xhtml-template.rb
+++ otros/secundarios/xhtml-template.rb
@@ -24,6 +24,23 @@ template = "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 
 end
 
+def htmlTemplateHead title = "Título", css = ""
+
+# Para el caso del css y el epub:type se sigue esta condición:
+#   la variable no está vacía ? output si sí : output si no
+# Se pone pegada a la línea anterior para evitar la creación de una línea vacía en caso de ser nil, y si no es el caso, se crea un salto de línea al inicia con \n
+
+template = "<!DOCTYPE html>
+<html lang=\"#{$lang}\">
+    <head>
+        <meta charset=\"UTF-8\" />
+        <title>#{title}</title>#{css != "" ? "\n        <link href=\"#{css}\" rel=\"stylesheet\" type=\"text/css\" />" : ""}
+    </head>
+    <body>
+"
+
+end
+
 $xhtmlTemplateFoot = "    </body>
 </html>
 "
